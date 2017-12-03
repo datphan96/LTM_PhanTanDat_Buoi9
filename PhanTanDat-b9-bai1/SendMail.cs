a54ec6d2f95984046cf4a717167fc7bf4e691ced
Đạt Phan <t Phan@DESKTOP-R9F1HPG>
2017-12-03 22:41:34 +0700

    Add project files.

---
+++ PhanTanDat-b9-bai1/SendMail.cs
@@ -0,0 +1,109 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Net;
+using System.Net.Mail;
+
+namespace PhanTanDat_b9_bai1
+{
+    public partial class SendMail : Form
+    {
+        public SendMail()
+        {
+            InitializeComponent();
+        }
+
+        private void SendMail_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox1_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox1 = new TextBox();
+            textBox1.MaxLength = 20;
+        }
+
+        private void textBox2_TextChanged(object sender, EventArgs e)
+        {
+            TextBox textBox2 = new TextBox();
+            textBox2.MaxLength = 8;
+            textBox2.PasswordChar = '*';
+        }
+
+        private void textBox3_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox4_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void textBox5_TextChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mailclient = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+
+                mailclient.From = new MailAddress("abc@gmail.com");
+                mailclient.To.Add("vtysh@gmail.com");
+                mailclient.Subject = "Test Mail";
+                mailclient.Body = "Testing SMTP mail from GMAIL";
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("abc@gmail.com", "p@assword");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mailclient);
+                MessageBox.Show("Mail sent successfully !");
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show(ex.ToString());
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            try
+            {
+                MailMessage mail = new MailMessage();
+                SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+                mail.From = new MailAddress("abc@gmail.com");
+                mail.To.Add("vtysh@gmail.com");
+                mail.Subject = "Test Mail 1";
+                mail.Body = "Mail with attachment";
+
+                System.Net.Mail.Attachment attachment;
+                attachment = new System.Net.Mail.Attachment("your attachment file");
+                mail.Attachments.Add(attachment);
+
+                SmtpServer.Port = 587;
+                SmtpServer.Credentials = new System.Net.NetworkCredential("abc@gmail.com", "p@assword");
+                SmtpServer.EnableSsl = true;
+
+                SmtpServer.Send(mail);
+                MessageBox.Show("Mail with attachment sent successfully !");
+            }
+            catch (Exception ex)
+            {
+                Console.WriteLine(ex.ToString());
+            }
+        }
+    }
+}
---
+++ PhanTanDat-b9-baimau/Program.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Net;
+using System.Net.Mail;
+
+namespace PhanTanDat_b9_baimau
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            SmtpClient mailclient = new SmtpClient("smtp.gmail.com", 587);
+            mailclient.EnableSsl = true;
+            mailclient.Credentials = new NetworkCredential("abc@gmail.com","p@assword");
+            MailMessage message = new MailMessage("abc@gmail.com","vtysh@gmail.com");
+            message.Subject = "Hello Client !";
+            message.Body = "Test mail client";
+            mailclient.Send(message);
+            Console.WriteLine("Mail sent successfully !");
+
+            //Gửi kèm tệp tin Attachments
+            SmtpClient SmtpServer = new SmtpClient("smtp.gmail.com");
+            MailMessage mail = new MailMessage();
+            mail.From = new MailAddress("abc@gmail.com");
+            mail.To.Add("vtysh@gmail.com");
+            mail.Subject = "Test mail attachment";
+            mail.Body = "Mail with attachment";
+            System.Net.Mail.Attachment attachment;
+            attachment = new System.Net.Mail.Attachment("d:/abc.txt");
+            mail.Attachments.Add(attachment);
+            SmtpServer.Port = 587;
+            SmtpServer.Credentials = new System.Net.NetworkCredential("abc@gmail.com", "p@assword");
+            SmtpServer.EnableSsl = true;
+            SmtpServer.Send(mail);
+            Console.WriteLine("Mail with attachment sent successfully !")
+        }
+    }
+}
+
